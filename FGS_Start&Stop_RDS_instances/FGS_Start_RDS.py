2dd54c06c997a706212c6e875af4a9babedf7024
Benjamin <39343981+Hasardine@users.noreply.github.com>
2022-09-18 16:57:44 +0200

    FGS_RDS_Start&Stop

---
+++ FGS_Start&Stop_RDS_instances/FGS_Start_RDS.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python
+# Author: Hasardine
+# Python 3.6
+
+# Start instance
+# APIG : https://rds.eu-west-0.prod-cloud-ocb.orange-business.com/v3/{project-id}/instances/{instance-id}/action/startup 
+
+import requests
+from requests.packages import urllib3
+
+
+# Account & password for dev
+account = "xxxx.xx"
+apipass = "xxxxxxxxx"
+domainID = "OCBxxxxxx"
+projectId = "xxxxxxxxxxxx"
+instanceId = "xxxxxxxxx"
+
+def handler (event, context):
+  
+  # ====================== Get API token From API password ======================
+  urlToken = "https://iam.eu-west-0.prod-cloud-ocb.orange-business.com/v3/auth/tokens"
+
+  payloadToken = "{\r\n    \"auth\": {\r\n        \"identity\": {\r\n            \"methods\": [\r\n                \"password\"\r\n            ],\r\n            \"password\": {\r\n                \"user\": {\r\n                    \"name\": \"" + account+"\" ,\r\n                    \"password\": \"" +apipass+ "\",\r\n                    \"domain\": {\r\n                        \"name\": \""+domainID+"\"\r\n                    }\r\n                }\r\n            }\r\n        },\r\n        \"scope\": {\r\n            \"project\": {\r\n                //big_data\r\n                \"id\": \""+projectId+"\"\r\n                }\r\n        }\r\n    }\r\n}"
+  headersToken = {
+    'ContentType': 'application/json;charset=utf8 ',
+    'Content-Type': 'text/plain'
+  }
+
+  responseToken = requests.request("POST", urlToken, headers=headersToken, data=payloadToken)
+
+  unscoped_token = responseToken.headers.get("x-subject-token") if "x-subject-token" in responseToken.headers.keys() else None
+
+  # ====================== Stop RDS Instance ======================
+
+
+  urlRds = "https://rds.eu-west-0.prod-cloud-ocb.orange-business.com/v3/"+projectId+"/instances/"+instanceId+"/action/startup"
+
+  headersRds = {
+    "content-type": "application/json",
+    "X-Auth-Token": unscoped_token,
+    "X-Language": "en-us"
+  }
+
+  responseRds = requests.request("POST", urlRds, headers=headersRds)
+
+  print(responseRds.text)
+
+
